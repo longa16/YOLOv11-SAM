1d41c0ecb0f2d9a64b23c720b50475e3c0c46ea1
longa16 <139324660+longa16@users.noreply.github.com>
2026-05-26 16:58:37 +0200

    Delete Desktop/app_calibrage_camera - maj directory

--- Desktop/app_calibrage_camera - maj/main.py
+++
@@ -1,7 +0,0 @@
-"""Page d'entrée de l'application de calibrage de caméra. Elle lance l'interface graphique principale."""
-
-from ui.main_window import ApplicationCalibrage
-
-if __name__ == '__main__':
-    app = ApplicationCalibrage()
-    app.mainloop()
